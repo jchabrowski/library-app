881450343c7069e9f2ee0b5a117dbb29bad5419f
Radosław Różański <r.rozanski@gmail.com>
2020-10-24 16:33:02 +0200

    Add JSON Server with some example data

--- src/components/api/book.api.js
+++ src/components/api/book.api.js
@@ -2,21 +2,21 @@
 // import BookContext from './bookContext';
 import Axios from 'axios';
 
-const BaseUrl = 'http://localhost:3010';
+const BaseUrl = 'http://localhost:3010/books';
 
 //get method, returning books, to be set with setState
 export const getBooks = () => {
-  return Axios.get(`${BaseUrl}/books`)
+  return Axios.get(`${BaseUrl}`)
     .catch(err => console.error(err))
 }
 
 export const handleDelete = (currentId) => {
-  return Axios.delete(`${BaseUrl}/book/${currentId}`)
+  return Axios.delete(`${BaseUrl}/${currentId}`)
     .catch(err => console.error(err))
 }
 
 export const handlePost = (title, author, pages, rating) => {
-  return Axios.post(`${BaseUrl}/book`, {
+  return Axios.post(`${BaseUrl}`, {
     title: title,
     author: author,
     pages: pages,
@@ -26,7 +26,7 @@ export const handlePost = (title, author, pages, rating) => {
 }
 
 export const handleBookEdit = (title, author, id, pages, rating) => {
-  return Axios.put(`${BaseUrl}/book/${id}`, {
+  return Axios.put(`${BaseUrl}/${id}`, {
     title: title,
     author: author,
     pages: pages,
